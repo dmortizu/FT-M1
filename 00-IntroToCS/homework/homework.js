252ac81e9a52de422d36d66848207af4c724bd37
dmortizu <dmortizu@gmail.com>
2021-09-15 22:00:57 -0500

    Terminado el HW 00

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -1,10 +1,24 @@
 function BinarioADecimal(num) {
   // tu codigo aca
+  var resultado=0;
+  var numero = num.split('').reverse();
+  for(var i=0; i<numero.length; i++){
+    resultado += (numero[i]*Math.pow(2,i));
+  }
+  return resultado;
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  if(num <= 0) return '0';
+  var array = [];
+
+  while(num > 0) {
+    array.unshift(num % 2);
+    num = Math.floor(num / 2);
+  }
+  return array.join('');
 
 }
 
